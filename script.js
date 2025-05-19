6b4b35d23f480a0af0ef2394beac8bfb9c5e6f71
honey <honeyyousaf9@gmail.com>
2025-05-19 14:16:51 +0500

    Initail commit of portfolio website

---
+++ script.js
@@ -0,0 +1,5 @@
+ let sidebar = document.querySelector("#sidebar")
+ let humbargur = document.querySelector("#humbargur")
+humbargur.addEventListener("click", () => {
+    sidebar.classList.toggle("active")
+})
